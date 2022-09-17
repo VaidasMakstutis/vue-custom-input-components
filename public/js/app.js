1efe100d1724b85b969f5662b77ef4b87769e9b4
Vaidas <vaidas.makstutis@gmail.com>
2022-09-17 21:37:20 +0300

    custom input component

---
+++ public/js/app.js
@@ -0,0 +1,34 @@
+Vue.component("coupon", {
+    props: ["code"],
+
+    template: `
+        <input type="text" :value="code" @input="updateCode($event.target.value)">
+    `,
+
+    data() {
+        return {
+            invalids: ["ALLFREE", "SOMETHINGELSE"],
+        };
+    },
+
+    methods: {
+        updateCode(code) {
+            // Validation
+            if (this.invalids.includes(code)) {
+                alert("This coupon is no longer valid!");
+
+                this.$refs.input.value = code = "";
+            }
+
+            this.$emit("input", code);
+        },
+    },
+});
+
+new Vue({
+    el: "#app",
+
+    data: {
+        coupon: "FREEBIE",
+    },
+});
